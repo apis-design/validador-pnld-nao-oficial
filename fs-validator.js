750c78ab715bf48f1476bb389bcf17036a2b19b3
design10 <design10@APIS-design-10.local>
2024-08-05 19:12:53 -0300

    change: fs-validator with testar

--- fs-validator.js
+++ fs-validator.js
@@ -15,64 +15,96 @@ import fs from "fs"
 
 use(chaiFs);
 
-const errors = [];
+const basePath = 'teste'
+let results = []
 
-const folderPath = 'teste'
-
-function checkDirectoryAndSubDirs(basePath, subDirs, files) {
+function testar(testDescription, testFunction) {
+    let status;
+    let errorMessage = null;
 
     try {
-        expect(basePath).to.be.a.directory(`Não possui o diretório ${basePath}`);
-    } catch (error) {
-        errors.push(error.message);
+        testFunction()
+        status = 'passed';
+    } catch (e) {
+        status = 'not passed';
+        errorMessage = e.message
     }
 
-    subDirs.forEach(subDir => {
-        const fullPath = path.join(basePath, subDir);
-        try {
-            expect(fullPath).to.be.a.directory(`Não possui o diretório ${fullPath}`);
-        } catch (error) {
-            errors.push(error.message);
+    results.push({
+        code: testDescription,
+        message: testDescription,
+        type: 'notice',
+        runnerExtras: {
+            status: status,
+            errorMessage: errorMessage
         }
     });
-
-    files.forEach(file => {
-        const fullPath = path.join(basePath, file);
-        try {
-            expect(fullPath).to.be.a.file(`Não possui o arquivo ${fullPath}`);
-        } catch (error) {
-            errors.push(error.message);
-        }
-    });
-
-    if (errors.length > 0) {
-        `Foram encontrados os seguintes erros:\n${errors.join('\n')}`
-    }
 }
 
-const filesToCheck = ['index.html', 'toc.npx', 'content.opf', 'cover.jpeg'];
-const dirsToCheck = ['resources', 'content', 'resources/images', 'resources/scripts', 'resources/styles', 'resources/videos', 'resources/audios', 'resources/extras'];
+testar('Verifica se o diretório base existe', () => {
+    expect(basePath).to.be.a.directory(`Não possui o diretório ${basePath}`);
+});
 
-checkDirectoryAndSubDirs(folderPath, [], filesToCheck);
-checkDirectoryAndSubDirs(folderPath, dirsToCheck, [])
+// 5.1 Estrutura
+testar('Verifica se o diretório resources existe', () => {
+    expect(path.join(basePath, 'resources')).to.be.a.directory(`Não possui o diretório ${path.join(basePath, 'resources')}.  Veja: 5.1 Estrutura`);
+});
+
+testar('Verifica se o diretório content existe', () => {
+    expect(path.join(basePath, 'content')).to.be.a.directory(`Não possui o diretório ${path.join(basePath, 'content')}  Veja: 5.1 Estrutura`);
+});
+
+testar('Verifica se o diretório resources/images existe', () => {
+    expect(path.join(basePath, 'resources/images')).to.be.a.directory(`Não possui o diretório ${path.join(basePath, 'resources/images')}.  Veja: 5.1 Estrutura`);
+});
+
+testar('Verifica se o diretório resources/scripts existe', () => {
+    expect(path.join(basePath, 'resources/scripts')).to.be.a.directory(`Não possui o diretório ${path.join(basePath, 'resources/scripts')}.  Veja: 5.1 Estrutura`);
+});
+
+testar('Verifica se o diretório resources/styles existe', () => {
+    expect(path.join(basePath, 'resources/styles')).to.be.a.directory(`Não possui o diretório ${path.join(basePath, 'resources/styles')}.  Veja: 5.1 Estrutura`);
+});
+
+testar('Verifica se o diretório resources/videos existe', () => {
+    expect(path.join(basePath, 'resources/videos')).to.be.a.directory(`Não possui o diretório ${path.join(basePath, 'resources/videos')}.  Veja: 5.1 Estrutura`);
+});
+
+testar('Verifica se o diretório resources/audios existe', () => {
+    expect(path.join(basePath, 'resources/audios')).to.be.a.directory(`Não possui o diretório ${path.join(basePath, 'resources/audios')}.  Veja: 5.1 Estrutura`);
+});
+
+testar('Verifica se o diretório resources/extras existe', () => {
+    expect(path.join(basePath, 'resources/extras')).to.be.a.directory(`Não possui o diretório ${path.join(basePath, 'resources/extras')}.  Veja: 5.1 Estrutura`);
+});
+
+// 5.4 Pasta de recursos
+testar('Verifica se o arquivo index.html existe', () => {
+    expect(path.join(basePath, 'index.html')).to.be.a.file(`Não possui o arquivo ${path.join(basePath, 'index.html')}. 5.4 Pasta de recursos`);
+});
+
+testar('Verifica se o arquivo toc.npx existe', () => {
+    expect(path.join(basePath, 'toc.npx')).to.be.a.file(`Não possui o arquivo ${path.join(basePath, 'toc.npx')}. 5.4 Pasta de recursos`);
+});
+
+testar('Verifica se o arquivo content.opf existe', () => {
+    expect(path.join(basePath, 'content.opf')).to.be.a.file(`Não possui o arquivo ${path.join(basePath, 'content.opf')}. 5.4 Pasta de recursos`);
+});
+
+testar('Verifica se o arquivo cover.jpeg existe', () => {
+    expect(path.join(basePath, 'cover.jpeg')).to.be.a.file(`Não possui o arquivo ${path.join(basePath, 'cover.jpeg')}. 5.4 Pasta de recursos`);
+});
 
 // 5.2 Nomenclatura
 // 5.2.1 Nomenclatura de pastas / arquivo
-// Todas as pastas adicionadas ao projeto deverão ser nomeadas utilizando caracteres
-// minúsculos, sem caracteres especiais e/ou acentos e separados por linha baixa "_" (underline),
-// não sendo permitido iniciar o nome com números.
-
-const nomenclatura_erro = `5.2 Nomenclatura 5.2.1 Nomenclatura de pastas Todas as pastas adicionadas ao projeto deverão ser nomeadas utilizando caracteres minúsculos, sem caracteres especiais e/ou acentos e separados por linha baixa "_" (underline), não sendo permitido iniciar o nome com números`
-
-const invalidNameRegex = /[^a-z0-9_]|^[0-9]/;
 
 function checkNamingConventions(basePath) {
     function checkName(name, fullPath) {
+        const invalidNameRegex = /[^a-z0-9_]|^[0-9]/;
+
         if (invalidNameRegex.test(name)) {
-            errors.push({
-                erro: `Nome inválido encontrado: ${name} em ${fullPath}`,
-                detalhe: nomenclatura_erro
-            });
+            // Lança o erro para ser capturado por `testar`
+            throw new Error(`Nome inválido encontrado: ${name} em ${fullPath}. Veja: 5.2 Nomenclatura`);
         }
     }
 
@@ -83,28 +115,24 @@ function checkNamingConventions(basePath) {
             const itemPath = path.join(directoryPath, item);
             const stats = fs.lstatSync(itemPath);
 
-            checkName(item, itemPath);
+            testar(`Verifica nomenclatura de ${item}`, () => checkName(item, itemPath));
 
             if (stats.isDirectory()) {
-                traverseDirectory(itemPath); // Verifica recursivamente subdiretórios
+                traverseDirectory(itemPath);
             }
-            // Arquivos também são validados com checkName
         });
     }
 
-    try {
+    testar('Verifica se o diretório base existe', () => {
         expect(basePath).to.be.a.directory(`Não possui o diretório ${basePath}`);
-        traverseDirectory(basePath);
-    } catch (error) {
-        errors.push(error.message);
-    }
+    });
 
-    if (errors.length > 0) {} else {
-        console.log('Todos os nomes de arquivos e diretórios estão em conformidade.');
-    }
+    traverseDirectory(basePath);
 }
 
-checkNamingConventions(folderPath);
 
 
-console.log(errors)
+checkNamingConventions(basePath);
+
+
+console.log(results)
