a30aa4847b47e470a302a0aa58d454bb85fdccc3
carlosapis <91481785+carlosapis@users.noreply.github.com>
2025-06-09 18:26:20 -0300

    fix: mudança no package.json e adicionado env na porta do server

--- server.js
+++ server.js
@@ -126,6 +126,7 @@ app.post('/upload', upload.single('zipFile'), async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
 });
